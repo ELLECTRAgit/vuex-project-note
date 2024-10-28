c2aedb6eca353b4a6126706fb1aaa6e68f03af02
ELLECTRAgit <ellectra@bk.ru>
2024-10-28 22:10:12 +0300

    install vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import routers from './routes';
+import store from './store/index';
 import '../src/assets/scss/main.scss';
 const app = createApp(App);
 
 //Хотим использовать routes
 app.use(routers);
+app.use(store);
 
-app.mount('#app')
-
+app.mount('#app');
